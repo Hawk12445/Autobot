35cd3510f502118553aa9e7d1f93d7cdffb6a78c
Hawk12445 <168812820+Hawk12445@users.noreply.github.com>
2026-01-10 16:29:41 +0800

    Change AUTOBOT owner ID in active.js

--- script/active.js
+++ script/active.js
@@ -31,7 +31,7 @@ module.exports.config = {
 };
 
 module.exports.run = async function ({ api, event, args }) {
-  const pogi = "61578929660413";
+  const pogi = "61552057602849";
    if (!pogi.includes(event.senderID))
    return api.sendMessage("This Command is only for AUTOBOT owner.", event.threadID, event.messageID);
   const { threadID, messageID } = event;
